f169b9f4efc0bedf78813f6492ed61c88b673e7b
andrewthegreat5 <andy123llk@gmail.com>
2019-06-13 11:54:22 +1200

    big cleanup

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function openNav() {
   document.getElementById("mySidenav").style.width = "20%";
   document.getElementById("mouse").style.left = "20%";
-  document.getElementById("body").style.margin = "0% 0% 0% 20%";
+  document.getElementById("body").style.margin = "0% 5% 0% 20%";
   document.getElementById("body").style.transform = " scale(0.8)";
-  document.getElementById("body2").style.margin = "0% 0% 0% 20%";
+  document.getElementById("body2").style.margin = "0% 5% 0% 20%";
   document.getElementById("body2").style.transform = " scale(0.8)";
-  document.getElementById("title").style.margin = "2% 5% 0% 29%";
-  document.getElementById("title").style.fontSize = "2.4vw";
+  document.getElementById("title").style.margin = "2% 5% 0% 22%";
+  document.getElementById("title").style.transform = " scale(0.8)";
 }
 
 function closeNav() {
@@ -17,5 +17,5 @@ function closeNav() {
   document.getElementById("body2").style.margin = "1% 5% 1% 10%";
   document.getElementById("body2").style.transform = " scale(1)";
   document.getElementById("title").style.margin = "2% 5% 0% 11%";
-  document.getElementById("title").style.fontSize = "3vw";
+  document.getElementById("title").style.transform = " scale(1)";
 }
