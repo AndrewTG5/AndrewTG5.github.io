8beca0c601c554cd20bfdaa17ce2e5dc0be2396c
andrewthegreat5 <andy123llk@gmail.com>
2019-06-28 14:47:22 +1200

    further refinements

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function openNav() {
-  document.getElementById("mySidenav").style.width = "20%";
-  document.getElementById("mouse").style.left = "20%";
+  document.getElementById("mySidenav").style.width = "22%";
+  document.getElementById("mouse").style.left = "22%";
 }
 
 function closeNav() {
