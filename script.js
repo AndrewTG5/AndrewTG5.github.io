8ffccdf05bfebadc72d953e680d0399901b263a6
andrewthegreat5 <andy123llk@gmail.com>
2019-07-03 10:45:54 +1200

    start adding code comments

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function openNav() {
-  document.getElementById("mySidenav").style.width = "22%";
-  document.getElementById("mouse").style.left = "22%";
+  document.getElementById("mySidenav").style.width = "22%"; //size of the menu when open
+  document.getElementById("mouse").style.left = "22%"; //moves the menu bar over with the menu
 }
 
 function closeNav() {
-  document.getElementById("mySidenav").style.width = "0%";
-  document.getElementById("mouse").style.left = "0%";
+  document.getElementById("mySidenav").style.width = "0%"; //size of the menu when closed
+  document.getElementById("mouse").style.left = "0%"; //moves the menu bar over with the menu
 }
