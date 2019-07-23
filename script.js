f6c1439af5ce7a5239bd5b87a80b58f44b339a1b
andrewthegreat5 <andy123llk@gmail.com>
2019-07-23 18:16:50 +1200

    finish new menu icon, need polish and comments

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function openNav() {
   var hamburger = document.querySelector(".hamburger");
-  hamburger.classList.toggle("is-active");
+  hamburger.classList.add("is-active");
   document.getElementById("mySidenav").style.width = "22%"; //size of the menu when open
   document.getElementById("mouse").style.left = "22%"; //moves the menu bar over with the menu
 }
 
 function closeNav() {
   var hamburger = document.querySelector(".hamburger");
-  hamburger.classList.toggle("is-active");
+  hamburger.classList.remove("is-active");
   document.getElementById("mySidenav").style.width = "0%"; //size of the menu when closed
   document.getElementById("mouse").style.left = "0%"; //moves the menu bar over with the menu
 }
