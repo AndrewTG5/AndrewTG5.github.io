b92eec2a5b1ff5266abbd60a061cb73bdcd3cd01
andrewthegreat5 <andy123llk@gmail.com>
2019-07-24 10:33:38 +1200

    made layers to get backdrop-filter: to work, might break something

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function openNav() {
   var hamburger = document.querySelector(".hamburger");
   hamburger.classList.add("is-active");
+  document.getElementById("mySideNavblur").style.width = "22%";
+  document.getElementById("mouseBlur").style.left = "22%";
   document.getElementById("mySidenav").style.width = "22%"; //size of the menu when open
   document.getElementById("mouse").style.left = "22%"; //moves the menu bar over with the menu
 }
@@ -8,6 +10,8 @@ function openNav() {
 function closeNav() {
   var hamburger = document.querySelector(".hamburger");
   hamburger.classList.remove("is-active");
+  document.getElementById("mySideNavblur").style.width = "0%";
+  document.getElementById("mouseBlur").style.left = "0%";
   document.getElementById("mySidenav").style.width = "0%"; //size of the menu when closed
   document.getElementById("mouse").style.left = "0%"; //moves the menu bar over with the menu
 }
