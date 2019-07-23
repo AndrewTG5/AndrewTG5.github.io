787355a54e26c36d926ed028be6b40f16bfd83b4
andrewthegreat5 <andy123llk@gmail.com>
2019-07-23 15:01:25 +1200

    new menu icon WIP

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 function openNav() {
+  var hamburger = document.querySelector(".hamburger");
+  hamburger.classList.toggle("is-active");
   document.getElementById("mySidenav").style.width = "22%"; //size of the menu when open
   document.getElementById("mouse").style.left = "22%"; //moves the menu bar over with the menu
 }
 
 function closeNav() {
+  var hamburger = document.querySelector(".hamburger");
+  hamburger.classList.toggle("is-active");
   document.getElementById("mySidenav").style.width = "0%"; //size of the menu when closed
   document.getElementById("mouse").style.left = "0%"; //moves the menu bar over with the menu
 }
