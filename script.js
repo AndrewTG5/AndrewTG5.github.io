6d2bf3f6bbe092374cd7040a108cd90d3351564d
andrewthegreat5 <andy123llk@gmail.com>
2019-07-25 09:26:15 +1200

    only need one extra blur element now + other changes

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
 function openNav() {
   var hamburger = document.querySelector(".hamburger");
-  hamburger.classList.add("is-active");
-  document.getElementById("mySideNavblur").style.width = "22%";
-  document.getElementById("mouseBlur").style.left = "22%";
+  hamburger.classList.add("is-active"); //triggers hamburger menu animation
+  document.getElementById("mouseBlur").style.left = "22%"; //moves the menu bar over with the menu
   document.getElementById("mySidenav").style.width = "22%"; //size of the menu when open
   document.getElementById("mouse").style.left = "22%"; //moves the menu bar over with the menu
 }
 
 function closeNav() {
   var hamburger = document.querySelector(".hamburger");
-  hamburger.classList.remove("is-active");
-  document.getElementById("mySideNavblur").style.width = "0%";
-  document.getElementById("mouseBlur").style.left = "0%";
+  hamburger.classList.remove("is-active"); //triggers hamburger menu animation
+  document.getElementById("mouseBlur").style.left = "0%"; //moves the menu bar over with the menu
   document.getElementById("mySidenav").style.width = "0%"; //size of the menu when closed
   document.getElementById("mouse").style.left = "0%"; //moves the menu bar over with the menu
 }
