102fd5ff3d1886d5300d649cd442ea4e2c157088
andrewthegreat5 <andy123llk@gmail.com>
2019-08-08 20:16:23 +1200

    small things

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function closeNav() {
   var hamburger = document.querySelector(".hamburger");
   hamburger.classList.remove("is-active"); //triggers hamburger menu animation
   document.getElementById("mySidenav").style.width = "0%"; //size of the menu when closed
-  document.getElementById("mouse").style.left = "0%"; //moves the menu bar over with the menu
-  document.getElementById("mouseBlur").style.left = "0%"; //moves the menu bar over with the menu
+  document.getElementById("mouse").style.left = "0"; //moves the menu bar over with the menu
+  document.getElementById("mouseBlur").style.left = "0"; //moves the menu bar over with the menu
   document.getElementById("mouse").style.boxShadow = "-4px 1px 15px -3px rgba(0,0,0,0.5)";
 }
