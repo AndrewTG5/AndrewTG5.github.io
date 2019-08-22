e990b5279b1c46371a4e4d545d719dfd8f09c76a
andrewthegreat5 <andy123llk@gmail.com>
2019-08-22 12:05:20 +1200

    add painfully jank theme toggle WIP

--- script.js
+++ script.js
@@ -15,3 +15,35 @@ function closeNav() {
   document.getElementById("mouseBlur").style.left = "0"; //moves the menu bar over with the menu
   document.getElementById("mouse").style.boxShadow = "-4px 1px 15px -3px rgba(0,0,0,0.5)";
 }
+
+function dark() {
+  var oldlink = document.getElementById("css");
+  var newlink = document.createElement("link");
+  var mode = document.getElementById("mode");
+  var light = "light()";
+  var dark = "toggle.css";
+
+  newlink.setAttribute("rel", "stylesheet");
+  newlink.setAttribute("href", dark);
+  newlink.setAttribute("id", "css");
+
+  mode.setAttribute("onclick", light);
+
+  document.getElementsByTagName("head").item(0).replaceChild(newlink, oldlink);
+}
+
+function light() {
+  var oldlink = document.getElementById("css");
+  var newlink = document.createElement("link");
+  var mode = document.getElementById("mode");
+  var dark = "dark()";
+  var light = "css.css";
+
+  newlink.setAttribute("rel", "stylesheet");
+  newlink.setAttribute("href", light);
+  newlink.setAttribute("id", "css");
+
+  mode.setAttribute("onclick", dark);
+
+  document.getElementsByTagName("head").item(0).replaceChild(newlink, oldlink);
+}
