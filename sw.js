31525bcf7d527b581145974416b08357767ffa07
andrewthegreat5 <andy123llk@gmail.com>
2019-08-28 20:54:52 +1200

    nope, hotfix 3

--- sw.js
+++ sw.js
@@ -2,8 +2,15 @@ self.addEventListener('install', function(e) {
   e.waitUntil(
     caches.open('airhorner').then(function(cache) {
       return cache.addAll([
-        '/',
-
+        './',
+        'index.html',
+        'Evolocity.html',
+        'Robotics.html',
+        'css.css',
+        'script.js',
+        'settings.html',
+        'browserconfig.xml',
+        'site.webmanifest'
       ]);
     })
   );
