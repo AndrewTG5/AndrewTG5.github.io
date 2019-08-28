6c8551386d9b0b5e300a68d7a344a31eda1d1fc0
andrewthegreat5 <andy123llk@gmail.com>
2019-08-28 20:45:04 +1200

    update service worker

--- sw.js
+++ sw.js
@@ -4,24 +4,20 @@ self.addEventListener('install', function(e) {
       return cache.addAll([
         './',
         'index.html',
+        'Evolocity.html',
+        'Robotics.html'
         'css.css',
         'script.js'
       ]);
     })
   );
 });
+
 self.addEventListener('fetch', function(event) {
-
   console.log(event.request.url);
-
   event.respondWith(
-
     caches.match(event.request).then(function(response) {
-
       return response || fetch(event.request);
-
     })
-
   );
-
 });
