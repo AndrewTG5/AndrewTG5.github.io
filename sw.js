3deb233009b130deb5ba36f936b03a3188970616
andrewthegreat5 <andy123llk@gmail.com>
2019-08-28 20:52:38 +1200

    experiment

--- sw.js
+++ sw.js
@@ -2,13 +2,8 @@ self.addEventListener('install', function(e) {
   e.waitUntil(
     caches.open('airhorner').then(function(cache) {
       return cache.addAll([
-        './',
-        'index.html',
-        'Evolocity.html',
-        'Robotics.html',
-        'css.css',
-        'script.js',
-        'settings.html'
+        '/',
+
       ]);
     })
   );
