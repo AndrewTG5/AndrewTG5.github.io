7d6414f1f45bcfc707ba443494609d9ed314a613
andrewthegreat5 <andy123llk@gmail.com>
2019-09-19 14:01:36 +1200

    sooner or later

--- service-worker.js
+++ service-worker.js
@@ -17,6 +17,6 @@ addEventListener('message', (event) => {
   }
 });
 
-const urls = [index.html, evolocity.html, robotics.html, settings.html, script.js, css.css];
+const urls = ['index.html, evolocity.html, robotics.html, settings.html, script.js, css.css'];
 const cacheName = 'main-cache';
 event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(urls)));
