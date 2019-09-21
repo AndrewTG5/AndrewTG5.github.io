3f05a0b401df8af75f592b85561bd94bfa4a6bba
andrewthegreat5 <andy123llk@gmail.com>
2019-09-21 22:15:23 +1200

    v1.5 beta 4, try to support chrome 78, not quite there

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ function openNav() {
   var soundPref = localStorage.getItem("soundPref");
   var hamburger = document.querySelector(".hamburger");
   hamburger.classList.add("is-active"); //triggers hamburger menu animation
-  document.getElementById("mySidenav").style.width = "22%"; //size of the menu when open
-  document.getElementById("mouse").style.left = "22%"; //moves the menu bar over with the menu
-  document.getElementById("mouseBlur").style.left = "22%"; //moves the menu bar over with the menu
+  document.getElementById("mySidenav").style.width = "22vw"; //size of the menu when open
+  document.getElementById("mouse").style.left = "22vw"; //moves the menu bar over with the menu
+  document.getElementById("mouseBlur").style.left = "22vw"; //moves the menu bar over with the menu
   document.getElementById("mouse").style.boxShadow = "4px 1px 15px -5px rgba(0,0,0,0.5), 50vw 0px 0px 50vw rgba(0,0,0,0.3)";
   if (navOpen == "0") {
     sessionStorage.setItem("navOpen", "1");
@@ -23,7 +23,7 @@ function closeNav() {
   var soundPref = localStorage.getItem("soundPref");
   var hamburger = document.querySelector(".hamburger");
   hamburger.classList.remove("is-active"); //triggers hamburger menu animation
-  document.getElementById("mySidenav").style.width = "0%"; //size of the menu when closed
+  document.getElementById("mySidenav").style.width = "0"; //size of the menu when closed
   document.getElementById("mouse").style.left = "0"; //moves the menu bar over with the menu
   document.getElementById("mouseBlur").style.left = "0"; //moves the menu bar over with the menu
   document.getElementById("mouse").style.boxShadow = "-4px 1px 15px -3px rgba(0,0,0,0.5), 50vw 0px 0px 50vw rgba(0,0,0,0)";
