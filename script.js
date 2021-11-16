eaa1850d94074a91d8e4038fae3733060762f6ef
andrewthegreat5 <andy123llk@gmail.com>
2021-11-16 16:21:24 +1300

    v6.1.0, generate notif with js

--- script.js
+++ script.js
@@ -30,9 +30,43 @@ function bgSelector(background) {
   localStorage.setItem('bgPref', background);
 }
 
-// eslint-disable-next-line no-unused-vars
 function createUIPrompt(message) {
-  const notif = document.getElementById('notif');
+  const notif = document.createElement('div');
+  notif.setAttribute("id", "notif");
+  notif.style.display = 'none';
+
+  const svgx = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+  svgx.setAttribute('xmlns','http://www.w3.org/2000/svg');
+  svgx.setAttribute('class','icon icon-tabler icon-tabler-x');
+  svgx.setAttribute('width','40');
+  svgx.setAttribute('height','40');
+  svgx.setAttribute('viewBox','0 0 24 24');
+  svgx.setAttribute('stroke-width','1');
+  svgx.setAttribute('stroke','currentColor');
+  svgx.setAttribute('fill','none');
+  svgx.setAttribute('stroke-linecap','round');
+  svgx.setAttribute('stroke-linejoin','round');
+  svgx.setAttribute('onclick','dismissUIPrompt()');
+  svgx.innerHTML = '<path stroke="none" d="M0 0h24v24H0z" /><line x1="18" y1="6" x2="6" y2="18" /><line x1="6" y1="6" x2="18" y2="18" />';
+
+  const svgc = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+  svgc.setAttribute('xmlns','http://www.w3.org/2000/svg');
+  svgc.setAttribute('class','icon icon-tabler icon-tabler-alert-circle');
+  svgc.setAttribute('width','24');
+  svgc.setAttribute('height','24');
+  svgc.setAttribute('viewBox','0 0 24 24');
+  svgc.setAttribute('stroke-width','1.5');
+  svgc.setAttribute('stroke','currentColor');
+  svgc.setAttribute('fill','none');
+  svgc.setAttribute('stroke-linecap','round');
+  svgc.setAttribute('stroke-linejoin','round');
+  svgc.innerHTML = '<path stroke="none" d="M0 0h24v24H0z" /><circle cx="12" cy="12" r="9" /><line x1="12" y1="8" x2="12" y2="12" /><line x1="12" y1="16" x2="12.01" y2="16" />';
+
+  notif.appendChild(svgx);
+  notif.appendChild(svgc);
+
+  document.body.appendChild(notif);
+
   notif.innerHTML += message;
   notif.style.display = 'initial';
   setTimeout(function() {
@@ -44,13 +78,13 @@ function createUIPrompt(message) {
   }, 5000);
 }
 
-// eslint-disable-next-line no-unused-vars
 function dismissUIPrompt() {
   const notif = document.getElementById('notif');
   notif.style.bottom = '-8vw';
   notif.style.opacity = '0';
   setTimeout(function() {
     notif.style.display = 'none';
+    notif.remove();
   }, 500);
 }
 
